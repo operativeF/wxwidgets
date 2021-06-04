d9eb1f60f0944e36413894f3fc6fd5a135d7083b
Thomas Figueroa <splines21@outlook.com>
2021-06-04 05:01:10 -0500

    Remove constexprized macro that was erroneous.

--- src/xrc/xh_tglbtn.cpp
+++ src/xrc/xh_tglbtn.cpp
@@ -14,7 +14,7 @@
 #if wxUSE_XRC && wxUSE_TOGGLEBTN
 
 # if !defined(__WXUNIVERSAL__) && !defined(__WXMOTIF__) && !(defined(__WXGTK__) && !defined(__WXGTK20__))
-static constexpr int wxHAVE_BITMAPS_IN_BUTTON = 1;
+#define wxHAVE_BITMAPS_IN_BUTTON 1;
 # endif
 
 #include "wx/xrc/xh_tglbtn.h"
