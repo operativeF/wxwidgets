0b7bf48bdafdde6eb79d7c3492311ebf8b19ea35
Thomas Figueroa <splines21@outlook.com>
2021-06-23 02:09:24 -0500

    Fix boolean initialized with nullptr.

--- include/wx/xrc/xh_editlbox.h
+++ include/wx/xrc/xh_editlbox.h
@@ -27,7 +27,7 @@ public:
     bool CanHandle(wxXmlNode *node) override;
 
 private:
-    bool m_insideBox{nullptr};
+    bool m_insideBox{false};
     wxArrayString m_items;
 
     wxDECLARE_DYNAMIC_CLASS(wxEditableListBoxXmlHandler);
