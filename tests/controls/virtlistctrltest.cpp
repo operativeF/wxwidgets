7175618007cff78e1116cbb7e2c8ac164e61825c
Thomas Figueroa <splines21@outlook.com>
2021-06-30 01:25:54 -0500

    Convert virtlistctrltest.cpp

--- tests/controls/virtlistctrltest.cpp
+++ tests/controls/virtlistctrltest.cpp
@@ -25,44 +25,7 @@
 #include "testableframe.h"
 #include "wx/uiaction.h"
 
-// ----------------------------------------------------------------------------
-// test class
-// ----------------------------------------------------------------------------
-
-class VirtListCtrlTestCase : public CppUnit::TestCase
-{
-public:
-    VirtListCtrlTestCase() { }
-
-    void setUp() override;
-    void tearDown() override;
-
-private:
-    CPPUNIT_TEST_SUITE( VirtListCtrlTestCase );
-        CPPUNIT_TEST( UpdateSelection );
-        WXUISIM_TEST( DeselectedEvent );
-    CPPUNIT_TEST_SUITE_END();
-
-    void UpdateSelection();
-    void DeselectedEvent();
-
-    wxListCtrl *m_list;
-
-    VirtListCtrlTestCase(const VirtListCtrlTestCase&) = delete;
-	VirtListCtrlTestCase& operator=(const VirtListCtrlTestCase&) = delete;
-};
-
-// register in the unnamed registry so that these tests are run by default
-CPPUNIT_TEST_SUITE_REGISTRATION( VirtListCtrlTestCase );
-
-// also include in its own registry so that these tests can be run alone
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( VirtListCtrlTestCase, "VirtListCtrlTestCase" );
-
-// ----------------------------------------------------------------------------
-// test initialization
-// ----------------------------------------------------------------------------
-
-void VirtListCtrlTestCase::setUp()
+TEST_CASE("Virtual list control test.")
 {
     // Define a class overriding OnGetItemText() which must be overridden for
     // any virtual list control.
@@ -71,8 +34,8 @@ void VirtListCtrlTestCase::setUp()
     public:
         VirtListCtrl()
             : wxListCtrl(wxTheApp->GetTopWindow(), wxID_ANY,
-                         wxPoint(0, 0), wxSize(400, 200),
-                         wxLC_REPORT | wxLC_VIRTUAL)
+                wxPoint(0, 0), wxSize(400, 200),
+                wxLC_REPORT | wxLC_VIRTUAL)
         {
         }
 
@@ -83,68 +46,65 @@ void VirtListCtrlTestCase::setUp()
         }
     };
 
-    m_list = new VirtListCtrl;
-}
+    auto m_list = new VirtListCtrl;
 
-void VirtListCtrlTestCase::tearDown()
-{
+    SUBCASE("UpdateSelection")
+    {
+        m_list->SetItemCount(10);
+        CHECK_EQ( 0, m_list->GetSelectedItemCount() );
+
+        m_list->SetItemState(7, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+        CHECK_EQ( 1, m_list->GetSelectedItemCount() );
+
+        m_list->SetItemState(0, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
+        CHECK_EQ( 2, m_list->GetSelectedItemCount() );
+
+        // The item 7 is now invalid and so shouldn't be counted as selected any
+        // more.
+        m_list->SetItemCount(5);
+        CHECK_EQ( 1, m_list->GetSelectedItemCount() );
+    }
+
+    SUBCASE("DeselectedEvent")
+    {
+    #if wxUSE_UIACTIONSIMULATOR
+        m_list->AppendColumn("Col0");
+        m_list->SetItemCount(1);
+        wxListCtrl* const list = m_list;
+
+        EventCounter selected(list, wxEVT_LIST_ITEM_SELECTED);
+        EventCounter deselected(list, wxEVT_LIST_ITEM_DESELECTED);
+
+        wxUIActionSimulator sim;
+
+        wxRect pos;
+        list->GetItemRect(0, pos);
+
+        //We move in slightly so we are not on the edge
+        wxPoint point = list->ClientToScreen(pos.GetPosition()) + wxPoint(10, 10);
+
+        sim.MouseMove(point);
+        wxYield();
+
+        sim.MouseClick();
+        wxYield();
+
+        // We want a point within the listctrl but below any items
+        point = list->ClientToScreen(pos.GetPosition()) + wxPoint(10, 50);
+
+        sim.MouseMove(point);
+        wxYield();
+
+        sim.MouseClick();
+        wxYield();
+
+        CHECK_EQ(1, selected.GetCount());
+        CHECK_EQ(1, deselected.GetCount());
+    #endif
+    }
+
     delete m_list;
     m_list = NULL;
-}
-
-void VirtListCtrlTestCase::UpdateSelection()
-{
-    m_list->SetItemCount(10);
-    CHECK_EQ( 0, m_list->GetSelectedItemCount() );
-
-    m_list->SetItemState(7, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
-    CHECK_EQ( 1, m_list->GetSelectedItemCount() );
-
-    m_list->SetItemState(0, wxLIST_STATE_SELECTED, wxLIST_STATE_SELECTED);
-    CHECK_EQ( 2, m_list->GetSelectedItemCount() );
-
-    // The item 7 is now invalid and so shouldn't be counted as selected any
-    // more.
-    m_list->SetItemCount(5);
-    CHECK_EQ( 1, m_list->GetSelectedItemCount() );
-}
-
-void VirtListCtrlTestCase::DeselectedEvent()
-{
-#if wxUSE_UIACTIONSIMULATOR
-    m_list->AppendColumn("Col0");
-    m_list->SetItemCount(1);
-    wxListCtrl* const list = m_list;
-
-    EventCounter selected(list, wxEVT_LIST_ITEM_SELECTED);
-    EventCounter deselected(list, wxEVT_LIST_ITEM_DESELECTED);
-
-    wxUIActionSimulator sim;
-
-    wxRect pos;
-    list->GetItemRect(0, pos);
-
-    //We move in slightly so we are not on the edge
-    wxPoint point = list->ClientToScreen(pos.GetPosition()) + wxPoint(10, 10);
-
-    sim.MouseMove(point);
-    wxYield();
-
-    sim.MouseClick();
-    wxYield();
-
-    // We want a point within the listctrl but below any items
-    point = list->ClientToScreen(pos.GetPosition()) + wxPoint(10, 50);
-
-    sim.MouseMove(point);
-    wxYield();
-
-    sim.MouseClick();
-    wxYield();
-
-    CHECK_EQ(1, selected.GetCount());
-    CHECK_EQ(1, deselected.GetCount());
-#endif
 }
 
 #endif // wxUSE_LISTCTRL
