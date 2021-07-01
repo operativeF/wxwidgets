b19ca990032194ed1914a4b2a4bf0295bafe6ab2
Thomas Figueroa <splines21@outlook.com>
2021-07-01 00:46:06 -0500

    Convert bitmaptogglebuttontest.cpp

--- tests/controls/bitmaptogglebuttontest.cpp
+++ tests/controls/bitmaptogglebuttontest.cpp
@@ -25,97 +25,63 @@
 #include "wx/uiaction.h"
 #include "wx/artprov.h"
 
-class BitmapToggleButtonTestCase : public CppUnit::TestCase
+TEST_CASE("Bitmap toggle button test")
 {
-public:
-    BitmapToggleButtonTestCase() { }
-
-    void setUp() override;
-    void tearDown() override;
-
-private:
-    CPPUNIT_TEST_SUITE( BitmapToggleButtonTestCase );
-        WXUISIM_TEST( Click );
-        CPPUNIT_TEST( Value );
-    CPPUNIT_TEST_SUITE_END();
-
-    void Click();
-    void Value();
-
-    wxBitmapToggleButton* m_button;
-
-    BitmapToggleButtonTestCase(const BitmapToggleButtonTestCase&) = delete;
-	BitmapToggleButtonTestCase& operator=(const BitmapToggleButtonTestCase&) = delete;
-};
-
-// register in the unnamed registry so that these tests are run by default
-CPPUNIT_TEST_SUITE_REGISTRATION( BitmapToggleButtonTestCase );
-
-// also include in its own registry so that these tests can be run alone
-CPPUNIT_TEST_SUITE_NAMED_REGISTRATION( BitmapToggleButtonTestCase,
-                                      "BitmapToggleButtonTestCase" );
-
-void BitmapToggleButtonTestCase::setUp()
-{
-    m_button = new wxBitmapToggleButton(wxTheApp->GetTopWindow(), wxID_ANY,
-                                        wxArtProvider::GetIcon(wxART_INFORMATION,
+    auto m_button = std::make_unique<wxBitmapToggleButton>(wxTheApp->GetTopWindow(),
+                                                           wxID_ANY,
+                                                           wxArtProvider::GetIcon(
+                                                               wxART_INFORMATION,
                                                                wxART_OTHER,
                                                                wxSize(32, 32)));
     m_button->Update();
     m_button->Refresh();
+
+    SUBCASE("Click")
+    {
+    #if wxUSE_UIACTIONSIMULATOR
+        EventCounter clicked(m_button.get(), wxEVT_TOGGLEBUTTON);
+
+        wxUIActionSimulator sim;
+
+        //We move in slightly to account for window decorations
+        sim.MouseMove(m_button->GetScreenPosition() + wxPoint(10, 10));
+        wxYield();
+
+        sim.MouseClick();
+        wxYield();
+
+        CHECK_EQ(1, clicked.GetCount());
+        CHECK(m_button->GetValue());
+
+        clicked.Clear();
+
+    #ifdef __WXMSW__
+        wxMilliSleep(1000);
+    #endif
+
+        sim.MouseClick();
+        wxYield();
+
+        CHECK_EQ(1, clicked.GetCount());
+        CHECK(!m_button->GetValue());
+    #endif // wxUSE_UIACTIONSIMULATOR
+    }
+
+    SUBCASE("Value")
+    {
+        EventCounter clicked(m_button.get(), wxEVT_BUTTON);
+
+        m_button->SetValue(true);
+
+        CHECK(m_button->GetValue());
+
+        m_button->SetValue(false);
+
+        CHECK(!m_button->GetValue());
+
+        CHECK_EQ( 0, clicked.GetCount() );
+    }
 }
-
-void BitmapToggleButtonTestCase::tearDown()
-{
-    wxDELETE(m_button);
-}
-
-void BitmapToggleButtonTestCase::Click()
-{
-#if wxUSE_UIACTIONSIMULATOR
-    EventCounter clicked(m_button, wxEVT_TOGGLEBUTTON);
-
-    wxUIActionSimulator sim;
-
-    //We move in slightly to account for window decorations
-    sim.MouseMove(m_button->GetScreenPosition() + wxPoint(10, 10));
-    wxYield();
-
-    sim.MouseClick();
-    wxYield();
-
-    CHECK_EQ(1, clicked.GetCount());
-    CHECK(m_button->GetValue());
-
-    clicked.Clear();
-
-#ifdef __WXMSW__
-    wxMilliSleep(1000);
-#endif
-
-    sim.MouseClick();
-    wxYield();
-
-    CHECK_EQ(1, clicked.GetCount());
-    CHECK(!m_button->GetValue());
-#endif // wxUSE_UIACTIONSIMULATOR
-}
-
-void BitmapToggleButtonTestCase::Value()
-{
-    EventCounter clicked(m_button, wxEVT_BUTTON);
-
-    m_button->SetValue(true);
-
-    CHECK(m_button->GetValue());
-
-    m_button->SetValue(false);
-
-    CHECK(!m_button->GetValue());
-
-    CHECK_EQ( 0, clicked.GetCount() );
-}
-
 #endif // wxHAS_BITMAPTOGGLEBUTTON
 
 #endif // wxUSE_TOGGLEBTN
