ccc3ee83dcfe63afc030de80593225216175dbb9
Thomas Figueroa <splines21@outlook.com>
2021-07-03 02:20:38 -0500

    Convert htmlboxtest.cpp

--- tests/controls/htmllboxtest.cpp
+++ tests/controls/htmllboxtest.cpp
@@ -20,40 +20,14 @@
 #include "wx/htmllbox.h"
 #include "itemcontainertest.h"
 
-class HtmlListBoxTestCase : public ItemContainerTestCase,
-                            public CppUnit::TestCase
+using HTMLBoxTest = ItemContainerTest<wxSimpleHtmlListBox>;
+
+TEST_CASE_FIXTURE(HTMLBoxTest, "HTML List Box test")
 {
-public:
-    HtmlListBoxTestCase() { }
+    auto m_container = std::make_unique<wxSimpleHtmlListBox>(
+        wxTheApp->GetTopWindow(), wxID_ANY);
 
-    void setUp() override;
-    void tearDown() override;
-
-private:
-    wxItemContainer *GetContainer() const override { return m_htmllbox; }
-    wxWindow *GetContainerWindow() const override { return m_htmllbox; }
-
-    CPPUNIT_TEST_SUITE( HtmlListBoxTestCase );
-        wxITEM_CONTAINER_TESTS();
-    CPPUNIT_TEST_SUITE_END();
-
-    wxSimpleHtmlListBox* m_htmllbox;
-
-    HtmlListBoxTestCase(const HtmlListBoxTestCase&) = delete;
-	HtmlListBoxTestCase& operator=(const HtmlListBoxTestCase&) = delete;
-};
-
-wxREGISTER_UNIT_TEST_WITH_TAGS(HtmlListBoxTestCase,
-                               "[HtmlListBoxTestCase][item-container]");
-
-void HtmlListBoxTestCase::setUp()
-{
-    m_htmllbox = new wxSimpleHtmlListBox(wxTheApp->GetTopWindow(), wxID_ANY);
-}
-
-void HtmlListBoxTestCase::tearDown()
-{
-    wxDELETE(m_htmllbox);
+    wxITEM_CONTAINER_TESTS();
 }
 
 #endif //wxUSE_HTML
