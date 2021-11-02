526caa2036c503340f9c3f9a8db7aca01a77ff35
Thomas Figueroa <splines21@outlook.com>
2021-11-02 16:47:23 -0500

    Add propgrid library back in.

---
+++ build/src/propgrid/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/wxWidgets/src/propgrid
+# Build directory: C:/dev/wxWidgets/build/src/propgrid
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
