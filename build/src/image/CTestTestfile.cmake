c39d0cf47a843e6de2db4820ffdefd0453e04bd4
Thomas Figueroa <splines21@outlook.com>
2021-11-02 22:12:15 -0500

    Remove /Zi from common windows flags.

---
+++ build/src/image/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/wxWidgets/src/image
+# Build directory: C:/dev/wxWidgets/build/src/image
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
