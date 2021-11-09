0dafbc76d4bfe5d2b19409df118f5095ccd661cb
Thomas Figueroa <splines21@outlook.com>
2021-11-09 14:57:34 -0600

    Remove automatically generated files that were accidentally committed.

--- build/src/image/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/wxWidgets/src/image
-# Build directory: C:/dev/wxWidgets/build/src/image
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
